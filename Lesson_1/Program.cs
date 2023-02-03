e84eec7825966abc0129453390d3b33f82f2ad7d
Ruslan Masalimov <myceu178@gmail.com>
2023-02-03 18:11:18 +0300

    Lesson 1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Console.Write("Введите ваше число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Ваше число: {number}");
+
+// Console.Write("Введите ваше число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Ваше число: {number * number}");
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 == number2 * number2)
+//     Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+// else
+// {
+//     Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+// }
+
+// Console.Write("Введите ваше число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int current_number = (-1) * number;
+// while (current_number <= number)
+// {
+//     Console.Write(current_number + " ");
+//     current_number++;
+// }
+
+// Console.Write("Введите положительное трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 99 && number < 1000)
+// {
+//     int digit3 = number % 10;
+//     Console.Write($"Последная цифра - {digit3}");
+// }
+// else
+// {
+//     Console.Write("Ваше число не трехзначное");
+// }
